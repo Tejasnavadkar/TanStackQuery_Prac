7508b6eb799c6327cdc704c89adb9b765070e06e
Tejasnavadkar <tejas.navadkar@gmail.com>
2025-01-26 10:13:35 +0530

    invalidate query using queryClient

--- client/src/App.js
+++ client/src/App.js
@@ -5,18 +5,20 @@ import { Form } from './components/Form';
 
 function App() {
 
-  const {data,isLoading} = useQuery({
-    queryKey:["todo"],   // usefull for cashing we can get cashed value through this key
-    queryFn:async ()=>{
-      await fetch('http://localhost:8000/todo')
-    },
+  const {data,status,isFetching} = useQuery({
+    queryKey:["todo"],   // usefull for cashing we can get cashed value or refetch using this key (the ['todo'] query key is a likely identifier for this query).
+    queryFn:async ()=> await (await fetch('http://localhost:8000/todo')).json() // These queries are cached by the query client to avoid unnecessary re-fetching of data.
+    ,
   })
+
+  if(isFetching) return <h1>Loading....</h1>
 
   console.log("Todo-Data--",data)
 
   return (
    <div>
        <Form/>
+       {data && data.data && data.data.map((todo)=> <li>{todo.title}</li>)}
    </div>
   );
 }
--- client/src/components/Form.jsx
+++ client/src/components/Form.jsx
@@ -1,26 +1,26 @@
-import { useMutation } from "@tanstack/react-query"
+import { useMutation, useQueryClient } from "@tanstack/react-query"
 import React, { useState } from "react"
 
-
+const CreateTodo = async (text) =>{
+    const res = await fetch('http://localhost:8000/todo/create',{
+         method:'post',
+         headers:{
+             'Content-Type':'application/json',
+ 
+         },
+         body: JSON.stringify({title:text})
+     })
+     if(res.ok){
+         console.log(res.json())
+     }
+    }
 
 export const Form = () =>{
 
-    const CreateTodo = async (text) =>{
-       const res = await fetch('http://localhost:8000/todo/create',{
-            method:'post',
-            headers:{
-                'Content-Type':'application/json',
-    
-            },
-            body: JSON.stringify({title:text})
-        })
-        if(res.ok){
-            console.log(res.bodyc)
-        }
-       }
+   
 
    const [text,setText] = useState("")
-
+   const queryClient = useQueryClient()
   
 
 //    const AddMutation = useMutation(CreateTodo(text),{
@@ -28,9 +28,19 @@ export const Form = () =>{
 //     onError:(error)=>console.log("err--",error)
 //    })
 
-const AddMutation = useMutation({
+const AddMutation = useMutation({   // used when we update or create in serverside 
     mutationFn:(text)=>CreateTodo(text),
-    onSuccess:()=> console.log("created"),
+    onSuccess:()=> {
+        console.log("created")
+        queryClient.invalidateQueries(['todo']) // the line queryClient.invalidateQueries(['todo']) is used to ensure that any query with the key ['todo'] gets invalidated and refetched after the mutation (the creation of a new todo) succeeds.
+    
+        // You trigger the mutation to create a new todo (todoMutation.mutate()).
+        // On success, the onSuccess callback is called.
+        // In this callback, queryClient.invalidateQueries(['todo']) is called to invalidate the ['todo'] query.
+        // The next time the app tries to read from the ['todo'] query, it will be refetched to include the newly created todo.
+        // This ensures that after a successful mutation (like creating a todo), your UI reflects the most up-to-date data.
+    
+    },
     onError:(error)=>console.log("err--",error)
 })
 
